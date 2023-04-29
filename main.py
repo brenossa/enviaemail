74dcfe315559a2f14c906b8d195896ed9722175a
Breno Dantas <39684269+brenossa@users.noreply.github.com>
2023-04-29 19:36:05 -0300

    Arquivo de interatividade
    Adicionado arquivo básico de interatividade para utilizar o enviaemail.

---
+++ main.py
@@ -0,0 +1,19 @@
+from modulos import envia_email
+
+credenciais = {"email_app":input("Digite seu email: "),
+               "senha_app":input("Digite sua senha: ")}
+destinatario = input("Digite o email de destino: ")
+corpo_msg = {"assunto":input("Assunto: "),
+             "mensagem":input("Mensagem: "),
+             "anexo":{"tipo":input("Tipo do arquivo: "),
+                      "caminho":input("Caminho do arquivo: ")} if input("Tem anexo? ").lower() in ["s","sim","tem"] else {}}
+
+tentativa = envia_email.EnviarEmail()
+tentativa.definir_credenciais(credenciais["email_app"], credenciais["senha_app"])
+tentativa.definir_destinatario(destinatario)
+tentativa.definir_mensagem(corpo_msg["assunto"], corpo_msg["mensagem"], corpo_msg["anexo"])
+
+if tentativa.enviar():
+    print("Email enviado com sucesso!!!")
+else:
+    print("Falha ao tentar enviar o email.")
