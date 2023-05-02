4c1f963dc1bcaeeca9d05931ab2e323645e927f1
Breno Dantas <39684269+brenossa@users.noreply.github.com>
2023-05-02 18:43:03 -0300

    Classe anexo
    Adicionada uma classe para criar anexos no formato correto.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ credenciais = {"email_app":input("Digite seu email: "),
                "senha_app":input("Digite sua senha: ")}
 destinatario = input("Digite o email de destino: ")
 corpo_msg = {"assunto":input("Assunto: "),
-             "mensagem":input("Mensagem: "),
-             "anexo":{"tipo":input("Tipo do arquivo: "),
-                      "caminho":input("Caminho do arquivo: ")} if input("Tem anexo? ").lower() in ["s","sim","tem"] else {}}
+             "mensagem":input("Mensagem: ")}
 
 tentativa = envia_email.EnviarEmail(envia_email.ServidorSMTP.gmail())
 tentativa.definir_credenciais(credenciais["email_app"], credenciais["senha_app"])
 tentativa.definir_destinatario(destinatario)
-tentativa.definir_mensagem(corpo_msg["assunto"], corpo_msg["mensagem"], corpo_msg["anexo"])
+tentativa.definir_mensagem(corpo_msg["assunto"], corpo_msg["mensagem"])
+tem_anexo = True if input("Tem anexo? ").lower() in ["s","sim","tem"] else False
+
+if tem_anexo:
+    anexo = envia_email.Anexo(input("Tipo do arquivo: "), input("Caminho do arquivo: "))
+    tentativa.inserir_anexo(anexo)
 
 if tentativa.enviar():
     print("Email enviado com sucesso!!!")
--- modulos/envia_email.py
+++ modulos/envia_email.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
 class ServidorSMTP:
-    def __init__(self, host, porta):
+    def __init__(self, host: str, porta: int):
         self.host = host
         self.porta = porta
     
@@ -13,22 +13,31 @@ class ServidorSMTP:
     def gmail(cls):
         return cls("smtp.gmail.com",587)
 
+class Anexo:
+    def __init__(self, tipo: str, caminho: str):
+        self.tipo = tipo
+        self.caminho = caminho
+
 class EnviarEmail:
-    def __init__(self, servidor):
+    def __init__(self, servidor: ServidorSMTP):
         self.host = servidor.host
         self.porta = servidor.porta
+        self.tem_anexo = False
 
-    def definir_credenciais(self, email, senha):
+    def definir_credenciais(self, email: str, senha: str):
         self.email = email
         self.senha = senha
 
-    def definir_destinatario(self, destinatario):
+    def definir_destinatario(self, destinatario: str):
         self.destinatario = destinatario
 
-    def definir_mensagem(self, assunto, mensagem, anexo):
+    def definir_mensagem(self, assunto: str, mensagem: str):
         self.assunto = assunto
         self.mensagem = mensagem
+
+    def inserir_anexo(self, anexo: Anexo):
         self.anexo = anexo
+        self.tem_anexo = True
 
     def enviar(self):
         msg = MIMEMultipart()
@@ -38,10 +47,10 @@ class EnviarEmail:
 
         msg.attach(MIMEText(self.mensagem, 'plain'))
 
-        if self.anexo.get("caminho"):
-            with open(self.anexo["caminho"], "rb") as anexo:
-                att = MIMEApplication(anexo.read(),_subtype=self.anexo["tipo"])
-                att.add_header("Content-Disposition","attachment",filename=str(os.path.basename(self.anexo["caminho"])))
+        if self.tem_anexo:
+            with open(self.anexo.caminho, "rb") as anexo:
+                att = MIMEApplication(anexo.read(),_subtype=self.anexo.tipo)
+                att.add_header("Content-Disposition","attachment",filename=str(os.path.basename(self.anexo.caminho)))
                 msg.attach(att)
 
         s = smtplib.SMTP(self.host, self.porta)
